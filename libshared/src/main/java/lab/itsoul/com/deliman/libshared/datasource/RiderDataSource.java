0cb8128b93def249e4ba9a49493658aefb01417b
mtowhidislam <m.towhid.islam@gmail.com>
2020-01-16 15:52:07 +0600

    Example Implementation Of a DataSource

--- libshared/src/main/java/lab/itsoul/com/deliman/libshared/datasource/RiderDataSource.java
+++ libshared/src/main/java/lab/itsoul/com/deliman/libshared/datasource/RiderDataSource.java
@@ -2,13 +2,51 @@ package lab.itsoul.com.deliman.libshared.datasource;
 
 import android.os.Handler;
 
-import com.itsoul.lab.android.data.base.DataSource;
+import com.itsoul.lab.android.data.base.DataStorage;
+import com.itsoul.lab.android.data.simple.SimpleDataSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 import lab.itsoul.com.deliman.libshared.model.Rider;
 
-public class RiderDataSource implements DataSource<Integer, Rider> {
+public class RiderDataSource extends SimpleDataSource<Integer, Rider> implements DataStorage {
+
+    public RiderDataSource(){
+        retrieve();
+    }
+
+    @Override
+    public void readAsynch(int offset, int pageSize, Consumer<Rider[]> consumer) {
+        if (consumer != null) {
+            new Handler().postDelayed(() -> {
+                        //TODO:
+                        List<Rider> items = Arrays.asList(readSynch(offset, pageSize));
+                        consumer.accept(items.toArray(new Rider[0]));
+                    }
+                    , 1000);
+        }
+    }
+
+    @Override
+    public void save(boolean asynch) {
+        //TODO: Save Data using Preferred Persistence Technology:
+    }
+
+    @Override
+    public boolean retrieve() {
+        //TODO: Retrieve Data using Preferred Persistence Technology:
+        if (getInMemoryStorage().size() <= 0){
+            int index = 0;
+            for (Rider rider : getDummyData()) {
+                getInMemoryStorage().put(index++, rider);
+            }
+        }
+        //
+        return true;
+    }
 
     private Rider[] getDummyData(){
         return new Rider[]{
@@ -18,9 +56,9 @@ public class RiderDataSource implements DataSource<Integer, Rider> {
     }
 
     @Override
-    public void readAsynch(int offset, int pageSize, Consumer<Rider[]> consumer) {
-        if (consumer != null) {
-            new Handler().postDelayed(() -> consumer.accept(getDummyData()), 1000);
-        }
+    public boolean delete() {
+        //TODO: Delete Data using Preferred Persistence Technology:
+        return false;
     }
+
 }
