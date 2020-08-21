c75e344c82da3e748f66fcd6160701d7ac8e7b5f
mtowhidislam <m.towhid.islam@gmail.com>
2020-08-21 20:06:30 +0600

    productFlavors adding machanism

--- app/src/main/java/lab/infoworks/starter/ui/app/StarterApp.java
+++ app/src/main/java/lab/infoworks/starter/ui/app/StarterApp.java
@@ -1,6 +1,9 @@
 package lab.infoworks.starter.ui.app;
 
 import android.app.Application;
+import android.util.Log;
+
+import lab.infoworks.starter.BuildConfig;
 
 public class StarterApp extends Application {
 
@@ -8,6 +11,7 @@ public class StarterApp extends Application {
     public void onCreate() {
         super.onCreate();
         //Initializing GeoTracker
+        Log.i("StarterApp", "API Gateway: " + BuildConfig.api_gateway);
         //
     }
 }
